9a69ab5fa111e25d8560a9bc1751b933e2d3a756
Alexandr G <alexandr@gavrisco.com>
2018-01-17 22:38:31 +0200

    Add module for package manager initialization

---
+++ emacs.d/lisp/init-package.el
@@ -0,0 +1,21 @@
+;; Setup package manager
+
+(require 'package)
+(setq package-enable-at-startup nil)
+
+(add-to-list
+ 'package-archives
+ '("melpa" . "http://melpa.org/packages/"))
+(add-to-list
+ 'package-archives
+ '("org" . "http://orgmode.org/elpa/"))
+
+;; Init package.el
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+(provide 'init-package)
